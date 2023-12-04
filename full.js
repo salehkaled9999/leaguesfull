bd923f7936fbcdddaf57389d9b151a2fc8e414aa
salehkaled9999 <151852281+salehkaled9999@users.noreply.github.com>
2023-12-04 10:19:14 +0200

    Create full.js

---
+++ full.js
@@ -0,0 +1,4 @@
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js'/>          
+<script src='https://raw.githack.com/salehkaled9999/leaguesfull/main/rank.js' type='text/javascript'/>
+<script src='https://raw.githack.com/salehkaled9999/leaguesfull/main/TeamList.js' type='text/javascript'/>          
+<script src='https://raw.githack.com/salehkaled9999/leaguesfull/main/Player%20Details.js' type='text/javascript'/>
